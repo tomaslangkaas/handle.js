b075ca7170652c0f54ce4c9331645c5dc8d62c3d
tomaslangkaas <tomaslangkaas@users.noreply.github.com>
2015-11-20 11:22:55 +0100

    Create handle.dev.js

---
+++ handle.dev.js
@@ -0,0 +1,19 @@
+var handle = (function(doc){
+  function addEvent(){}
+  
+  return {
+    'key': function(){},
+    'click': function(id, fn){
+      var elm = doc['getElementById'](id) || {};
+			elm['onclick'] = fn;
+			elm['ontouchstart'] = function(e){
+				e['preventDefault']();
+				fn();
+			};
+			return function(){
+			  elm['onclick'] = elm['ontouchstart'] = null;
+			}
+    },
+    'state': function(){}
+  };
+})(document)
