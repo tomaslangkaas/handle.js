8f15b9c217fbcc008ff9ad132ee6ab0b43b454bd
tomaslangkaas <tomaslangkaas@users.noreply.github.com>
2015-11-20 11:30:41 +0100

    Update handle.dev.js

--- handle.dev.js
+++ handle.dev.js
@@ -1,19 +1,23 @@
 var handle = (function(doc){
-  function addEvent(){}
-  
-  return {
-    'key': function(){},
-    'click': function(id, fn){
-    	var elm = doc['getElementById'](id) || {};
-	elm['onclick'] = fn;
-	elm['ontouchstart'] = function(e){
-		e['preventDefault']();
-		fn();
+	function addEvent(){}
+	return {
+		'SHIFT': 4,
+		'CTRL': 2,
+		'ALT': 1,
+		'key': function(context, fn, key, modifiers){
+			
+		},
+		'click': function(id, fn){
+			var elm = doc['getElementById'](id) || {};
+			elm['onclick'] = fn;
+			elm['ontouchstart'] = function(e){
+				e['preventDefault']();
+				fn();
+			};
+			return function(){
+				elm['onclick'] = elm['ontouchstart'] = null;
+			}
+		},
+		'state': function(){}
 	};
-	return function(){
-	  elm['onclick'] = elm['ontouchstart'] = null;
-	}
-    },
-    'state': function(){}
-  };
 })(document)
